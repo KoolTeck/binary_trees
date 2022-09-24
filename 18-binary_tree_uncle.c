5ddb40b1af76cddcd4542770f07f169cc01c8ddb
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-09-24 03:59:57 -0700

    updates

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,21 +8,9 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-if (node == NULL)
+if (node == NULL || !node->parent || !node->parent->parent)
 return (NULL);
-if (node  == node->parent->left && node->parent->parent)
-{
-if (node->parent->parent->left)
-{
-return (node->parent->parent->left);
-}
-}
-if (node  == node->parent->right && node->parent->parent)
-{
-if (node->parent->parent->right)
-{
+if (node->parent  == node->parent->parent->left)
 return (node->parent->parent->right);
-}
-}
-return (NULL);
+return (node->parent->parent->left);
 }
